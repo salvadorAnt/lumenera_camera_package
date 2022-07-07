53e22c8966802c577838ac26a5a3260b5fe3f031
Farhad Dalirani <dalirani.1373@gmail.com>
2022-07-07 12:10:22 -0400

    seprate thread for each camera

--- src/livePreview.cpp
+++ src/livePreview.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <thread>
 
 #include <lucamapi.h>
 #include <lucamerr.h>
@@ -23,8 +24,59 @@ using namespace cv;
 using std::cout;
 using std::endl;
 
+void publish_camera_on_topic(std::vector<Camera> cameras, const std::vector<ros::Publisher> publishers, const int camera_index)
+{ 
+  ROS_INFO_STREAM( "Start thread for " << camera_index << "." << std::endl);
+
+  int frameSize;
+  BYTE *imagePtr;
+
+  // frame id
+  int frame_id = 0;
+
+  cv_bridge::CvImage img_bridge;
+  sensor_msgs::Image img_msg;
+
+  while (ros::ok()) {
+    // Grab and display a single image from each camera
+    
+    imagePtr = cameras[camera_index].getRawImage();
+
+    frameSize = cameras[camera_index].getFrameSize();
+
+    cameras[camera_index].createRGBImage(imagePtr,frameSize);
+
+    unsigned char* pImage = cameras[camera_index].getImage();
+    if (NULL != pImage) {
+
+      Mat image(cameras[camera_index].getMatSize(), CV_8UC3, pImage, Mat::AUTO_STEP);
+
+      // release asap
+      cameras[camera_index].releaseImage(); 
+
+      //cvtColor(image, image, CV_BGR2RGB,3);
+
+      // publish on ROS topic
+      std_msgs::Header header; // empty header
+      header.seq = frame_id; // user defined counter
+      header.stamp = ros::Time::now(); // time
+      img_bridge = cv_bridge::CvImage(header, sensor_msgs::image_encodings::RGB8, image);
+      img_bridge.toImageMsg(img_msg); // from cv_bridge to sensor_msgs::Image
+      publishers[camera_index].publish(img_msg); // ros::Publisher pub_img = node.advertise<sensor_msgs::Image>("topic", queuesize);
+
+    }
+
+    // increase frame Id
+    frame_id = frame_id + 1;
+  }
+
+  ROS_INFO_STREAM( "ROS closing for thread of camera " << camera_index << " recieved." << std::endl);
+}
+
 int main( int argc, char** argv )
 {
+  std::thread * th;
+
   // ROS node name
   ros::init(argc, argv, "lumenera_camera_node");
 	
@@ -38,11 +90,8 @@ int main( int argc, char** argv )
   ROS_INFO("Node: [lumenera_camera_node] has been started.");
 
 
-  // frame id
-  int frame_id = 0;
 
-  BYTE *imagePtr;
-  int frameSize;
+  
   bool displaySobelImage = false;
   if (argc > 1) {
     displaySobelImage = true;
@@ -86,61 +135,25 @@ int main( int argc, char** argv )
     sprintf(topic_name, "/lumenera_camera_package/%d", i + 1);
     publishers.push_back(nh.advertise<sensor_msgs::Image>(topic_name, 10));
   }
-
-  cv_bridge::CvImage img_bridge;
-  sensor_msgs::Image img_msg;
-
-  while (ros::ok()) {
-    // Grab and display a single image from each camera
-    for (size_t i = 0; i < cameras.size(); i++) {
-
-      imagePtr = cameras[i].getRawImage();
-
-      frameSize = cameras[i].getFrameSize();
-
-      cameras[i].createRGBImage(imagePtr,frameSize);
-
-      unsigned char* pImage = cameras[i].getImage();
-      if (NULL != pImage) {
-
-        Mat image(cameras[i].getMatSize(), CV_8UC3, pImage, Mat::AUTO_STEP);
-
-        cameras[i].releaseImage(); // release asap
-
-        //cvtColor(image, image, CV_BGR2RGB,3);
-
-        // publish on ROS topic
-        std_msgs::Header header; // empty header
-        header.seq = frame_id; // user defined counter
-        header.stamp = ros::Time::now(); // time
-        img_bridge = cv_bridge::CvImage(header, sensor_msgs::image_encodings::RGB8, image);
-        img_bridge.toImageMsg(img_msg); // from cv_bridge to sensor_msgs::Image
-        publishers[i].publish(img_msg); // ros::Publisher pub_img = node.advertise<sensor_msgs::Image>("topic", queuesize);
-
-        // Show the actual image
-        //resize(image, image, cameras[i].getDisplaySize());
-        //imshow(cameras[i].unprocessedWindowName(), image );
-
-      }
-      
-    }
-
-    // increase frame Id
-    frame_id = frame_id + 1;
-
-    // wait for a key for 1 ms. If 'q' key pressed, quit program
-    //int keyPressed = waitKey(1);
-    //if (keyPressed == 'q') {
-    //  ROS_INFO_STREAM("Quitting..." << endl);
-    //  break; 
-    //}
-    //rate.sleep();
-    
+  
+  // work with each camera on a seprate thread
+  std::vector<std::thread *> thread_vector;
+  for(size_t camera_index=0; camera_index < cameras.size(); camera_index++) {
+    th = new std::thread;
+    *th = std::thread(publish_camera_on_topic, cameras, publishers, camera_index);
+    thread_vector.push_back(th);
   }
 
-  
-  for(size_t i=0; i < cameras.size(); i++) {
-    cameras[i].stopStreaming();
+  ros::spin();
+
+  for(size_t camera_index=0; camera_index < cameras.size(); camera_index++) {
+    thread_vector[camera_index]->join();
+    delete thread_vector[camera_index];
+  }
+
+
+  for(size_t camera_index=0; camera_index < cameras.size(); camera_index++) {
+    cameras[camera_index].stopStreaming();
   }
 
   ROS_INFO("Node: [lumenera_camera_node] has been Ended.");
